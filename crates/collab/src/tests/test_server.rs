4b3633721c75db47f8420113e13a57ff611787de
Kyle Kelley <rgbkrk@gmail.com>
2024-04-23 09:54:25 -0700

    create a supermaven_api_key in collab config

--- crates/collab/src/tests/test_server.rs
+++ crates/collab/src/tests/test_server.rs
@@ -648,6 +648,7 @@ impl TestServer {
                 auto_join_channel_id: None,
                 migrations_path: None,
                 seed_path: None,
+                supermaven_api_key: None,
             },
         })
     }
